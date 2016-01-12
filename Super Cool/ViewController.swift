8d71e2f7bc531ea8975cf6d801e4a6745aad2cf8
Gabriel Bureau <gabriel.bureau777@gmail.con>
2016-01-12 16:34:44 -0500

    Initial Commit

---
+++ Super Cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Cool
+//
+//  Created by Gabriel Bureau on 2016-01-12.
+//  Copyright © 2016 Global United. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
